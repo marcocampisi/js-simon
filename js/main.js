de9ed83fbaa0c50d278ea5d431a46ac6753bf180
marcocampisi <marco.campisi@outlook.it>
2023-06-23 16:03:59 +0200

    Add main game functions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const simonContainer = document.querySelector('.numbersContainer');
 let simonNumbers = [];
+let userNumbers = [];
 
 function generateNumbers(simonNumbers) {
     for (let i = 0; i <  5; i++) {
@@ -7,3 +8,20 @@ function generateNumbers(simonNumbers) {
         simonNumbers.push(randomNum);
     }
 }
+
+function displayNumbers(simonContainer, simonNumbers) {
+    for (let i = 0; i < simonNumbers.length; i++) {
+        simonContainer.innerHTML += simonNumbers[i];
+    }
+    alert('Ricorda questi numeri, hai 30 secondi!');
+}
+
+function checkUserInput(userNumbers) {
+    for (let i = 0; i < 5; i++) {
+        let userInput = parseInt(prompt(`Inserisci il numero ${i + 1}`));
+        userNumbers.push(userInput);
+    }
+}
+
+generateNumbers(simonNumbers);
+displayNumbers(simonContainer, simonNumbers);
